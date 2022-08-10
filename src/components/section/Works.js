717a9b0efba2236017c8dd04eef65095d27d74be
surajDongre-16 <surajdongre8@gmail.com>
2022-08-11 00:40:49 +0530

    project-cards css fixed

--- src/components/section/Works.js
+++ src/components/section/Works.js
@@ -26,19 +26,6 @@ const Works = () => {
             ))}
         </div>
       </div>
-
-      {/* <div className="third">
-                <>
-                    <div className="pp-head-line mx-auto text-center">
-                        <h1 id="Projects" className="red-line pp-head">{section4Title}</h1>
-                    </div>
-                </>
-                <div className="row">
-                    {miscellaneous && miscellaneous.map((x) => 
-                    <Project key={x.id} id={x.id} url={x.url} name={x.name} />
-                    )}
-                </div>
-            </div> */}
     </>
   );
 };
