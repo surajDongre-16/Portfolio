03e1e95130284b07a564eb007b8fececb6fafae0
surajDongre-16 <surajdongre8@gmail.com>
2022-08-17 23:58:33 +0530

    header button changed to resume btn

--- src/components/layouts/HeaderButton.js
+++ src/components/layouts/HeaderButton.js
@@ -1,14 +1,45 @@
 import React from "react";
+import { social } from "../../profile";
 
 const HeaderButton = () => {
   return (
     <>
-      <div className="mx-auto">
-        <a href="#about">
+      <div className="mx-auto head-contact">
+        <a
+          title="Download Resume"
+          href={social.resume}
+          rel="noreferrer"
+          target="_blank"
+          download
+        >
           <button className="see">
-            Checkout Portfolio<i className="fas fa-eye"></i>
+            Resume<i className="fas fa-eye"></i>
           </button>
         </a>
+        {social.linkedin && (
+          <a
+            title="Visit Linkedin profile"
+            rel="noopener noreferrer"
+            target="_blank"
+            href={social.linkedin}
+          >
+            <button className="see seelink1">
+              <i className="fab fa-linkedin"></i>
+            </button>
+          </a>
+        )}
+        {social.github && (
+          <a
+            title="Visit Github profile"
+            rel="noopener noreferrer"
+            target="_blank"
+            href={social.github}
+          >
+            <button className="see seelink2">
+              <i className="fab fa-github"></i>
+            </button>
+          </a>
+        )}
       </div>
     </>
   );
